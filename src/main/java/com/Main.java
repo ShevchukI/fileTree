dde8f226897c086c61723bb3f02f2178981487cc
ShevchukI <ShevchukI>
2021-01-11 18:07:43 +0200

    Added OutputThread

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,14 +3,15 @@ package com;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.EnumSet;
+import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         Scanner input = new Scanner(System.in);
-        Stack<Path> result = new Stack<>();
 
         System.out.println("Enter root path:");
         Path rootPath = Paths.get(input.nextLine());
@@ -21,20 +22,36 @@ public class Main {
         System.out.println("Enter mask:");
         input.nextLine();
         String mask = input.nextLine();
-        
-        Files.walkFileTree(rootPath, EnumSet.noneOf(FileVisitOption.class), depth, new SimpleFileVisitor<Path>(){
+        long time = System.nanoTime();
+
+        BlockingQueue<Path> result = new LinkedBlockingQueue<>();
+
+        OutputThread consumer = new OutputThread(result);
+        Thread thread = new Thread(consumer);
+        thread.start();
+
+        Files.walkFileTree(rootPath, EnumSet.noneOf(FileVisitOption.class), depth, new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if(file.getFileName().toString().contains(mask)){
-                    result.push(file);
+                if (file.getFileName().toString().contains(mask)) {
+                    try {
+                        result.put(file);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+//                    result.push(file);
+
                 }
                 return FileVisitResult.CONTINUE;
             }
         });
 
-        while (!result.empty()){
-            System.out.println(result.pop().toAbsolutePath());
-        }
+        consumer.terminate();
 
+
+        time = System.nanoTime() - time;
+        System.out.printf("Elapsed %,9.3f ms\n", time/1_000_000.0);
     }
+
+
 }
